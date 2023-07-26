a5f2ca41635841fe0e2fbc17da73e07ddf2b2581
shane <shanechastain1@ymail.com>
2023-07-26 14:46:41 -0400

    parent done

---
+++ PointofSale/ItemType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointofSale
+{
+    internal class ItemType
+    {
+        //properties
+        public decimal Price { get; set; }
+        //Small, Medium, Large
+        public string Size { get; set; }
+        //public bool Shipped { get; set; }
+
+        //constructors
+        public ItemType(decimal _price, string _size)
+        {
+            Price = _price;
+            //Shipped = _shipped;
+            Size = _size;
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return $"{Price}";
+        }
+
+        public string Shipped(string size)
+        {
+            if (size == "Large")
+            {
+                return "Do you want this item shipped?";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+    }
+}
--- PointofSale/Program.cs
+++ PointofSale/Program.cs
@@ -1,1 +1,33 @@
-﻿afds
+﻿//Thrift store
+// parent item - bool big or small (+ shipping)       decimal price
+// 
+
+// child classes : Clothes(color, size, type, season),
+// furniture(type, material(wood, leather, cloth)),
+// Kitchenware(type(cutlery, dinnerware, utensils, tupperware)),      //five items per class
+// Electronics(type, year, brand),
+// Instruments(type(percussion, string, woodwind, brass), brand(Gibson, Fender, Yamaha, Kawai)) 
+
+//make List for items
+//have user choose child via number
+//have user choose ttem via number and quantity
+//provide total
+//re display menu and complete purchase or purchase more
+//subtotal, sales tax, grand total
+
+//ask for payent type cash, credit, or check
+//For cash, ask for amount tendered and provide change.
+//For check, get the check number.
+//For credit, get the credit card number, expiration, and CVV.
+
+//display receipt, items, totals, 
+
+
+
+
+
+
+
+
+//if time try storing in file
+//(Moderate) Store your list of products in a text file and then include an option to add to the product list, which then outputs to the product file.
