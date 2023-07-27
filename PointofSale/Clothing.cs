c8da9448c82ee0b6bb341a8fed0cc9e279bfa86f
Maya Araquil <mayaaraquil775@gmail.com>
2023-07-27 01:04:56 -0400

    updated

---
+++ PointofSale/Clothing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointofSale
+{
+    internal class Clothing: Merchandise
+    {
+        //properties
+        public string Color { get; set; }
+        public string Measurements { get; set; }
+        public string Season { get; set; }
+
+        // Clothing(type,season, Measurement, color)
+
+        //constructors
+
+        public Clothing(decimal _price, string _size, string _type, string _season, string _measurements, string _color) : base(_price, _size, _type)
+        {
+            Color = _color;
+            Measurements = _measurements;
+            Season = _season;
+        }
+
+        //methods
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{Color} {Measurements} {Season}";
+        }
+
+    }
+}
